ed86a70e2a50863ba3e7481eabb6c2302fe97312
Davinder Singh <ds.mudhar@gmail.com>
2018-04-05 03:37:59 +0100

    print DV table only when updated
    improved variable names
    removed useless debug info

--- node.cpp
+++ node.cpp
@@ -20,14 +20,14 @@ NodeRouter::NodeRouter(char node) {
     setup_connection(
         HOME_ADDR,
         std::to_string(this->port));
-
+/*
     for (int i = 0; i < routing_table.size(); i++) {
         std::cout << routing_table[i].router_id << " "
                   << routing_table[i].cost << " "
                   << routing_table[i].port << " "
                   //<< routing_table[i].next_router_port 
                   << std::endl;
-    }
+    }*/
 
     run_advertisement_thread();
 
@@ -67,16 +67,16 @@ void print_routing_table(NodeRouter *node) {
 void update_dv_in_table(NodeRouter *node, Packet *packet) {
     int new_cost;
     char to_router_id;
+    bool has_updated_table = false; //if added or updated the entry, used for printing
 
     to_router_id = packet->data[0];
-    std::cout << "new cost data " << packet->data << std::endl;
-
     std::string cost_str = packet->data.substr(1);
     new_cost = atoi((char *)cost_str.c_str());
 
     //trying to update routing table.
     pthread_mutex_lock(&node->mutex_routing_table); //lock it
 
+    //TODO can optimize if we sum the cost of this link in the node that's sending the DV. then we don't need to find the src (which is neighbor) in table
     RoutingTableNode *src_rtn = NULL; //src, neighbor from where dv came
     for (int i = 0; i < node->routing_table.size(); i++) {
         RoutingTableNode &rtn = node->routing_table.at(i);
@@ -85,25 +85,26 @@ void update_dv_in_table(NodeRouter *node, Packet *packet) {
         }
     }
 
-    std::cout << "new cost to " << to_router_id << " is " << cost_str << " from " << src_rtn->router_id << std::endl;
+    //std::cout << "new cost to " << to_router_id << " is " << cost_str << " from " << src_rtn->router_id << std::endl;
 
-    bool found = false; //router already exists
+    bool has_node = false; //table has node info, it's updated instead of adding new one
     for (int i = 0; i < node->routing_table.size(); i++) {
         RoutingTableNode &rtn = node->routing_table.at(i);
         if (rtn.router_id == to_router_id) {
-            found = true;
+            has_node = true;
 
             if (src_rtn->cost + new_cost < rtn.cost) {
                 std::cout << "updating new cost to " << src_rtn->cost + new_cost << " of " << to_router_id << " in table of " << rtn.router_id << ". Received from " << src_rtn->router_id << std::endl;
                 rtn.cost = src_rtn->cost + new_cost;
                 rtn.ref_router_id = src_rtn->router_id;
+                has_updated_table = true;
                 break;
             }
         }
     }
 
     //add if it's a distant router.
-    if (!found && to_router_id != node->node_id) {
+    if (!has_node && to_router_id != node->node_id) {
         RoutingTableNode rtn;
         rtn.is_neighbor = false;
         rtn.router_id = to_router_id;
@@ -111,11 +112,14 @@ void update_dv_in_table(NodeRouter *node, Packet *packet) {
         rtn.ref_router_id = packet->src_id;
         rtn.port = 0;
         node->routing_table.push_back(rtn);
+
+        has_updated_table = true;
     }
 
     pthread_mutex_unlock(&node->mutex_routing_table); //unlock it
 
-    print_routing_table(node);
+    if (has_updated_table)
+        print_routing_table(node);
 }
 
 void forward_message(NodeRouter *node, Packet &packet) {
@@ -171,46 +175,42 @@ goto_forward:
  * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  */
 void NodeRouter::handle_packet(Packet &packet, std::string message) {
-    std::cout << "Received: " << message << std::endl;
+    //std::cout << "Received: " << message << std::endl;
 
     if (message.length() < 2) {
         std::cout << "Invalid Packet Received. Message Length(): " << message.length() << std::endl;
         return;
     }
-    //deserialize packet.
+    //deserialize packet first
     packet.type = message[0];
     packet.dest_id = message[1];
     packet.src_id = message[2];
     packet.data = message.substr(3);
-    std::cout << "type: " << packet.type << std::endl;
-    std::cout << "dest_id: " << packet.dest_id << std::endl;
-    std::cout << "src_id: " << packet.src_id << std::endl;
-    std::cout << "data: " << packet.data << std::endl;
+    //std::cout << "type: " << packet.type << std::endl;
+    //std::cout << "dest_id: " << packet.dest_id << std::endl;
+    //std::cout << "src_id: " << packet.src_id << std::endl;
+    //std::cout << "data: " << packet.data << std::endl;
 
     if (packet.dest_id != node_id) {
         //message = packet_queue.back().dest_id;
         //message = message + " " + packet_queue.back().message;
         std::cout << "FORWARDING MESSSAGE" << std::endl;
         forward_message(this, packet);
-
-
     } else {
-        std::cout << "Final Destination: " << message << std::endl;
 
         switch (packet.type) {
             case HEADER_FIELD_TYPE_UPDATE_DV:
-                std::cout << "HEADER_FIELD_UPDATE_DV" << std::endl;
-
                 update_dv_in_table(this, &packet);
                 break;
             case HEADER_FIELD_TYPE_MSG:
+                std::cout << "Final Destination: " << message << std::endl;
+
                 std::cout << "HEADER_FIELD_TYPE_MSG" << std::endl;
                 break;
             default:
                 std::cout << "HEADER_FIELD_TYPE_INVALID" << std::endl;
         }
     }
-    //packet_queue.pop_back();
 }
 
 void *adv_thread_func(void *args) {
@@ -224,28 +224,28 @@ void *adv_thread_func(void *args) {
         pthread_mutex_lock(&node->mutex_routing_table);
 
         for (int i = 0; i < node->routing_table.size(); i++) {
-            RoutingTableNode rtn_edge = node->routing_table[i];
-            if (!rtn_edge.is_neighbor)
-                continue;
+            RoutingTableNode rtn_neighbor = node->routing_table[i];
+            if (!rtn_neighbor.is_neighbor)
+                continue; //don't send to distant router, their neighbors will send their info
 
             for (int j = 0; j < node->routing_table.size(); j++) {
                 RoutingTableNode rtn = node->routing_table[j];
                 //if (rtn.is_neighbor)
-                //    continue; //don't send change of dv of neighbor, they are fixed for simplicity. remove this for support.
+                //    continue; 
 
                 Packet packet;
                 packet.type = HEADER_FIELD_TYPE_UPDATE_DV;
-                packet.dest_id = rtn_edge.router_id; //dest is neighbor
-                packet.src_id = node->node_id;
 
                 //packet.data = std::to_string(rtn.router_id);// ...to reach the router
                 packet.data = ((char)rtn.router_id);
                 packet.data.append(std::to_string(rtn.cost)); //cost of link...
-                //packet.data += "\n";
                 //std::cout << "sending to " << packet.dest_id << " DATA: " << packet.data << " from " << packet.src_id << std::endl;
 
+                packet.src_id = node->node_id; //this router
+                packet.dest_id = rtn_neighbor.router_id; //neighbor is dest
+
                 std::string message = node->serialize_packet(&packet);
-                node->send_udp(message, HOME_ADDR, std::to_string(rtn_edge.port)); //send to neighbor (tc port)
+                node->send_udp(message, HOME_ADDR, std::to_string(rtn_neighbor.port)); //send to neighbor (tc port)
             }
         }
 
