2ddcfb28cb610b29031c4f9325fc24ebba7b0c9a
Davinder Singh <ds.mudhar@gmail.com>
2018-04-20 22:59:08 +0100

    enable sending to any dest node while testing

--- my-router.cpp
+++ my-router.cpp
@@ -7,7 +7,8 @@ bool DEBUG;
 
 void run_injecter() {
     std::string buffer, temp;
-    std::string server, port = "10000";
+    std::string server, port = "10000"; //inject to A.
+    char node_dest = 'D'; //ultimate destination
     Connection connection;
 
     connection.setup_connection((char *)"127.0.0.1", NODE_H_PORT);
@@ -16,7 +17,11 @@ void run_injecter() {
         //std::cout << "Sending message.";
 
         //std::cout << "Enter message followed by $: ";
-        buffer = "2DHThe quick brown fox jumps over the lazy dog.";
+        buffer = "2"; //TODO use struct Packet.
+        std::cout << "Enter node destination: " << std::endl;
+        std::cin >> node_dest;
+        buffer += node_dest;
+        buffer += "HThe quick brown fox jumps over the lazy dog.";
         buffer += " on ";
         buffer += std::to_string(std::time(0));
         /*temp = "";
@@ -44,7 +49,7 @@ int main(int argc, char *argv[]) {
         std::cout << "USAGE: " << argv[0] << " " << "<node> [<debug mode>]" << std::endl;
         std::cout << std::endl;
         std::cout << "       " << "<node> can be one from topology.dat " << std::endl;
-        std::cout << "       " << "use <node> = H for injecting message to node A" << std::endl;
+        std::cout << "       " << "use <node> = H for injecting message to dest node" << std::endl;
         std::cout << "       " << "use <debug mode> = 1 for verbose printing everything in terminal instead of writing to file" << std::endl;
         return 1;
     }
